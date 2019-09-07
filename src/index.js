72bbbd8acb053d1530aba52d38457071ddfa875b
jonyuan <jonyuan08@gmail.com>
2019-09-07 20:49:37 +0100

    finished game logic

--- src/index.js
+++ src/index.js
@@ -263,28 +263,6 @@ class Game extends React.Component {
   }
 }
 
-function calculateWinner(nodes) {
-  // const numNodes = nodes.length;
-  // const sideLen = Math.sqrt(numNodes);
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (nodes[a] && nodes[a] === nodes[b] && nodes[a] === nodes[c]) {
-      return nodes[a];
-    }
-  }
-  return null;
-}
-
 // ========================================
 
 ReactDOM.render(
